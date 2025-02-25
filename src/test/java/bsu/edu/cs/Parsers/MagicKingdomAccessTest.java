a4a117a01d06fa5a7e4678259e2ee85165fe0daa
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2025-02-25 13:21:38 -0500

    Added more tests to ParkParserTest.java to create more coverage. Also added a class to test to make sure we can access the sample file that will be used with the ParkParser.

---
+++ src/test/java/bsu/edu/cs/Parsers/MagicKingdomAccessTest.java
@@ -0,0 +1,43 @@
+package bsu.edu.cs.Parsers;
+
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+public class MagicKingdomAccessTest {
+    @Test
+    public void testAccessToJsonFile(){
+        String jsonData = readSampleFileAsString();
+        Assertions.assertNotNull(jsonData);
+    }
+
+    @Test
+    public void testCountRevisionsWithJsonPath(){
+        String jsonData = readSampleFileAsString();
+        JSONArray parks = getParksFromJson(jsonData);
+        Assertions.assertEquals(132, parks.size());
+    }
+
+    private String readSampleFileAsString(){
+        String output = "";
+        try{
+            try (InputStream sampleFile = Thread.currentThread().getContextClassLoader()
+                    .getResourceAsStream("parks.json")) {
+                output = new String(Objects.requireNonNull(sampleFile).readAllBytes(), Charset.defaultCharset());
+            }
+        }catch (IOException | NullPointerException e) {
+            System.err.println("Couldn't find sample file!");
+        }
+        return output;
+    }
+
+    private JSONArray getParksFromJson(String jsonData) {
+        return JsonPath.read(jsonData, "$..parks[*]");
+    }
+}
--- src/test/java/bsu/edu/cs/Parsers/ParkParserTest.java
+++ src/test/java/bsu/edu/cs/Parsers/ParkParserTest.java
@@ -6,11 +6,10 @@ import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParkParserTest {
 
@@ -30,39 +29,6 @@ public class ParkParserTest {
         JSONArray parsedRevisions = parkParser.extractData(new ByteArrayInputStream(parkParser.inputStreamInstance.inputStream));
         List<Park> convertedList = parkParser.convertRevisionsToList(parsedRevisions);
         assertEquals(132, convertedList.size());
-    }
-
-    @Test
-    public void extractDataTest() throws openInputStreamException, noItemFoundException {
-        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("parks.json");
-        assert sampleFile != null;
-        ParkParser parkParser = new ParkParser(new ApiInputStream(sampleFile));
-        assertNotNull(parkParser.extractData(sampleFile));
-    }
-
-    @Test
-    public void extractDataTestNoItemsFound() throws openInputStreamException, IOException {
-        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("parks.json");
-        assert sampleFile != null;
-        ParkParser parkParser = new ParkParser(new ApiInputStream(sampleFile));
-
-        //Closed the file to throw an error
-        sampleFile.close();
-
-        try {
-            parkParser.extractData(sampleFile);
-            fail("Expected noItemFoundExcetpion");
-        }catch(noItemFoundException e){
-            assertEquals("Could not find the item you were looking for!",e.getMessage());
-        }
-    }
-
-    @Test
-    public void parseTest() throws openInputStreamException, noItemFoundException {
-        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("parks.json");
-        assert sampleFile != null;
-        ParkParser parkParser = new ParkParser(new ApiInputStream(sampleFile));
-        assertEquals(132,parkParser.parse().size());
     }
 
 }
