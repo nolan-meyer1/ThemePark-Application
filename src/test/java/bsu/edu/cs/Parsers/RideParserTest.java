c6d3af282ff731a372ab1d7ec694c0e7810d8d2c
DJCoggs2005 <dakota.coughlin@bsu.edu>
2025-03-02 21:08:01 -0500

    Finished RideParserTest class.

---
+++ src/test/java/bsu/edu/cs/Parsers/RideParserTest.java
@@ -0,0 +1,34 @@
+package bsu.edu.cs.Parsers;
+
+import bsu.edu.cs.Exceptions.noItemFoundException;
+import bsu.edu.cs.Exceptions.openInputStreamException;
+import net.minidev.json.JSONArray;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RideParserTest {
+
+    @Test
+    public void getQueryTest() throws openInputStreamException {
+        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("parks.json");
+        assert sampleFile != null;
+        RideParser rideParser = new RideParser(new ApiInputStream(sampleFile));
+        assertEquals("$..rides[*]",rideParser.getQuery());
+    }
+
+    @Test
+    public void convertRevisionsToListTest() throws openInputStreamException, noItemFoundException {
+        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("parks.json");
+        assert sampleFile != null;
+        RideParser rideParser = new RideParser(new ApiInputStream(sampleFile));
+        JSONArray parsedRevisions = rideParser.extractData(new ByteArrayInputStream(rideParser.inputStreamInstance.inputStream));
+        List<Ride> convertedList = rideParser.convertRevisionsToList(parsedRevisions);
+        assertEquals(0, convertedList.size());
+    }
+
+}
