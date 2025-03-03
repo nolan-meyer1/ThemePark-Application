25e3a736d071f98203957934cf747f2c956cbc02
DJCoggs2005 <dakota.coughlin@bsu.edu>
2025-03-02 21:07:25 -0500

    Changes to RideConnection class to better serve tests.

--- src/main/java/bsu/edu/cs/InternetConnections/RideConnection.java
+++ src/main/java/bsu/edu/cs/InternetConnections/RideConnection.java
@@ -1,10 +1,14 @@
 package bsu.edu.cs.InternetConnections;
 
 public class RideConnection extends InternetConnection<Integer>{
+    private Integer searchItem;
 
     @Override
-    protected String createRequestUrl(Integer searchItem) {
+    public String createRequestUrl(Integer searchItem) {
         //Parameter doesn't need to be used because this simply is fetching one item
         return "https://queue-times.com/parks/"+searchItem+"/queue_times.json";
+    }
+    public Integer getSearchItem() {
+        return searchItem;
     }
 }
