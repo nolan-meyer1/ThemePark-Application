d7abc9bb3bba70567b7f4816ae73588ed9c1caf1
DJCoggs2005 <dakota.coughlin@bsu.edu>
2025-03-18 21:38:41 -0400

    Added Unit tests

--- src/test/java/bsu/edu/cs/Parsers/RideParserTest.java
+++ src/test/java/bsu/edu/cs/Parsers/RideParserTest.java
@@ -6,19 +6,20 @@ import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RideParserTest {
 
     @Test
     public void getQueryTest() throws openInputStreamException {
-        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("parks.json");
+        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("magicKingdom.json");
         assert sampleFile != null;
         RideParser rideParser = new RideParser(new ApiInputStream(sampleFile));
-        assertEquals("$..rides[*]",rideParser.getQuery());
+        assertEquals("$..rides[*]", rideParser.getQuery());
     }
 
     @Test
@@ -31,4 +32,30 @@ public class RideParserTest {
         assertEquals(44, convertedList.size());
     }
 
+    @Test
+    public void extractDataTest() throws openInputStreamException, noItemFoundException {
+        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("magicKingdom.json");
+        assert sampleFile != null;
+        ParkParser parkParser = new ParkParser(new ApiInputStream(sampleFile));
+        JSONArray parsedRevisions = parkParser.extractData(new ByteArrayInputStream(parkParser.inputStreamInstance.inputStream));
+        assertNotNull(parsedRevisions);
+    }
+
+    @Test
+    public void extractDataTestNoItems() throws openInputStreamException, IOException {
+        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("magicKingdom.json");
+        assert sampleFile != null;
+        ParkParser parkParser = new ParkParser(new ApiInputStream(sampleFile));
+
+        //Closed to test file exception
+        sampleFile.close();
+
+        try {
+            parkParser.extractData(sampleFile);
+            fail("Expected noItemFoundException");
+        } catch (noItemFoundException e) {
+            assertEquals("Could not find the item you were looking for!", e.getMessage());
+        }
+
+    }
 }
