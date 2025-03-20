542417cd42d8da311eae4019d8af13dc2ee4b4cb
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2025-03-19 22:24:43 -0400

    Made minor name refactor

--- src/test/java/bsu/edu/cs/Parsers/RidePositionParserTest.java
+++ src/test/java/bsu/edu/cs/Parsers/RidePositionParserTest.java
@@ -18,17 +18,17 @@ public class RidePositionParserTest {
     public void getQueryTest() throws openInputStreamException {
         InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("ridePositionSample.json");
         assert sampleFile != null;
-        RidePositionParser ridePositionParser = new RidePositionParser(new ApiInputStream(sampleFile));
-        assertEquals("$.[*]",ridePositionParser.getQuery());
+        RidePositionParser ridePositionParserInstance = new RidePositionParser(new ApiInputStream(sampleFile));
+        assertEquals("$.[*]",ridePositionParserInstance.getQuery());
     }
 
     @Test
     public void convertDataTest() throws openInputStreamException, noItemFoundException {
         InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("ridePositionSample.json");
         assert sampleFile != null;
-        RidePositionParser ridePositionParser = new RidePositionParser(new ApiInputStream(sampleFile));
-        JSONArray parsedData = ridePositionParser.extractData(new ByteArrayInputStream(ridePositionParser.inputStreamInstance.inputStream));
-        Coordinates coordinates = ridePositionParser.convertData(parsedData);
+        RidePositionParser ridePositionParserInstance = new RidePositionParser(new ApiInputStream(sampleFile));
+        JSONArray parsedData = ridePositionParserInstance.extractData(new ByteArrayInputStream(ridePositionParserInstance.inputStreamInstance.inputStream));
+        Coordinates coordinates = ridePositionParserInstance.convertData(parsedData);
         assertEquals("28.41772285 -81.58484892884152", coordinates.getLatitude() + " " + coordinates.getLongitude());
     }
 
@@ -36,8 +36,8 @@ public class RidePositionParserTest {
     public void extractDataTest() throws openInputStreamException, noItemFoundException {
         InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("ridePositionSample.json");
         assert sampleFile != null;
-        RidePositionParser ridePositionParser = new RidePositionParser(new ApiInputStream(sampleFile));
-        JSONArray parsedRevisions = ridePositionParser.extractData(new ByteArrayInputStream(ridePositionParser.inputStreamInstance.inputStream));
+        RidePositionParser ridePositionParserInstance = new RidePositionParser(new ApiInputStream(sampleFile));
+        JSONArray parsedRevisions = ridePositionParserInstance.extractData(new ByteArrayInputStream(ridePositionParserInstance.inputStreamInstance.inputStream));
         assertNotNull(parsedRevisions);
     }
 
@@ -45,13 +45,13 @@ public class RidePositionParserTest {
     public void extractDataTestNoItems() throws openInputStreamException, IOException {
         InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("ridePositionSample.json");
         assert sampleFile != null;
-        RidePositionParser ridePositionParser = new RidePositionParser(new ApiInputStream(sampleFile));
+        RidePositionParser ridePositionParserInstance = new RidePositionParser(new ApiInputStream(sampleFile));
 
         //Closed to test file exception
         sampleFile.close();
 
         try {
-            ridePositionParser.extractData(sampleFile);
+            ridePositionParserInstance.extractData(sampleFile);
             fail("Expected noItemFoundException");
         }catch (noItemFoundException e){
             assertEquals("Could not find the item you were looking for!",e.getMessage());
@@ -62,8 +62,8 @@ public class RidePositionParserTest {
     public void parseTest() throws openInputStreamException, noItemFoundException {
         InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("ridePositionSample.json");
         assert sampleFile != null;
-        RidePositionParser ridePositionParser = new RidePositionParser(new ApiInputStream(sampleFile));
-        assertInstanceOf(Coordinates.class,ridePositionParser.parse());
+        RidePositionParser ridePositionParserInstance = new RidePositionParser(new ApiInputStream(sampleFile));
+        assertInstanceOf(Coordinates.class,ridePositionParserInstance.parse());
     }
 
 }
