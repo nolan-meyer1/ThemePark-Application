a2b2ffbd543ce509285d9ed288a7a552afc976ae
DJCoggs2005 <dakota.coughlin@bsu.edu>
2025-03-19 21:18:52 -0400

    Updated naming conventions to follow CC standards in ControllerTest.

--- src/test/java/bsu/edu/cs/GUI/ControllerTest.java
+++ src/test/java/bsu/edu/cs/GUI/ControllerTest.java
@@ -7,43 +7,43 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ControllerTest {
 
     @Test
-    public void convertMinToHoursLessThan60MinTest(){
+    public void convertToHoursLessThan60MinTest(){
         Controller controller = new Controller();
         assertEquals("45 min",controller.convertMinToHours(45));
     }
 
     @Test
-    public void convertMinToHoursGreaterThan60MinTest(){
+    public void convertToHoursGreaterThan60MinTest(){
         Controller controller = new Controller();
         assertEquals("1 hr 30 min",controller.convertMinToHours(90));
     }
 
     @Test
-    public void convertMinToHoursTest(){
+    public void convertToHoursTest(){
         Controller controller = new Controller();
         assertEquals("2 hr",controller.convertMinToHours(120));
     }
 
     @Test
-    public void getWaitTimeColorLessThan45minTest(){
+    public void getWaitColorLessThan45Test(){
         Controller controller = new Controller();
         assertEquals("low-wait-time",controller.getWaitTimeColor(43));
     }
 
     @Test
-    public void getWaitTimeColorLessThan90minTest(){
+    public void getWaitColorLessThan90Test(){
         Controller controller = new Controller();
         assertEquals("medium-wait-time",controller.getWaitTimeColor(67));
     }
 
     @Test
-    public void getWaitTimeColorEqualTo90minTest(){
+    public void getWaitColorEqualTo90Test(){
         Controller controller = new Controller();
         assertEquals("medium-wait-time",controller.getWaitTimeColor(90));
     }
 
     @Test
-    public void getWaitTimeColorGreaterThan90minTest(){
+    public void getWaitColorGreaterThan90Test(){
         Controller controller = new Controller();
         assertEquals("high-wait-time",controller.getWaitTimeColor(100));
     }
