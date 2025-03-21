2fc0c5567821850903eb6e9156cdc80ea4837320
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2025-03-21 14:18:49 -0400

    Refactored unit tests to get rid of the APIKey

--- src/test/java/bsu/edu/cs/InternetConnections/ReviewConnectionTest.java
+++ src/test/java/bsu/edu/cs/InternetConnections/ReviewConnectionTest.java
@@ -2,7 +2,7 @@ package bsu.edu.cs.InternetConnections;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ReviewConnectionTest {
 
@@ -10,8 +10,8 @@ public class ReviewConnectionTest {
     public void createURLRequestTest() {
         ReviewConnection reviewConnectionInstance = new ReviewConnection();
 
-        assertEquals("https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJgUulalN-3YgRGoTaWM2LawY&fields=name,rating,reviews&key=AIzaSyD6cucMnd_mVIEq-3gdhJhrJqEFXAXjBeU",
-                reviewConnectionInstance.createRequestUrl("ChIJgUulalN-3YgRGoTaWM2LawY"));
+        assertTrue(reviewConnectionInstance.createRequestUrl("ChIJgUulalN-3YgRGoTaWM2LawY")
+                .matches("https://maps\\.googleapis\\.com/maps/api/place/details/json\\?place_id=ChIJgUulalN-3YgRGoTaWM2LawY&fields=name,rating,reviews&key=.+"));
     }
 
 }
--- src/test/java/bsu/edu/cs/InternetConnections/WeatherConnectionTest.java
+++ src/test/java/bsu/edu/cs/InternetConnections/WeatherConnectionTest.java
@@ -3,7 +3,7 @@ package bsu.edu.cs.InternetConnections;
 import bsu.edu.cs.Parsers.Coordinates;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WeatherConnectionTest {
 
@@ -11,7 +11,8 @@ public class WeatherConnectionTest {
     public void createURLRequestTest() {
         WeatherConnection weatherConnectionInstance = new WeatherConnection();
 
-        assertEquals("https://api.openweathermap.org/data/2.5/weather?lat=28.417663&lon=-81.581212&appid=1b0f4529f6b03a65738f138508968d2b&units=imperial",
-                weatherConnectionInstance.createRequestUrl(new Coordinates("28.417663","-81.581212")));
+        assertTrue(weatherConnectionInstance.createRequestUrl(new Coordinates("28.417663", "-81.581212")).matches(
+                "https://api\\.openweathermap\\.org/data/2\\.5/weather\\?lat=28\\.417663&lon=-81\\.581212&appid=.+"));
     }
+
 }
