4ce1a5bb2d652136283a7fd3c76f8ea5f68456ac
Chibuike Anyiam <anyiamchibuike3@gmail.com>
2025-03-24 11:53:36 -0400

    created the structure for the BoundingBoxParser

--- src/main/java/bsu/edu/cs/Parsers/BoundingBoxParser.java
+++ src/main/java/bsu/edu/cs/Parsers/BoundingBoxParser.java
@@ -2,7 +2,9 @@ package bsu.edu.cs.Parsers;
 
 import net.minidev.json.JSONArray;
 
-public class BoundingBoxParser extends Parser<Coordinates> {
+import java.util.LinkedHashMap;
+
+public class BoundingBoxParser extends Parser<BoundingBox> {
 
     public BoundingBoxParser(ApiInputStream inputStream) {
         super(inputStream);
@@ -14,7 +16,29 @@ public class BoundingBoxParser extends Parser<Coordinates> {
     }
 
     @Override
-    protected Coordinates convertData(JSONArray list) {
-        return null;
+    protected BoundingBox convertData(JSONArray list) {
+        String minimumLatitude = "";
+        String maximumLatitude = "";
+        String minimumLongitude = "";
+        String maximumLongitude = "";
+
+        if (list.get(0) instanceof LinkedHashMap<?, ?>) {
+            @SuppressWarnings("unchecked")
+            LinkedHashMap<String, ?> hashMapConverted = (LinkedHashMap<String, ?>) list.get(0);
+
+            if(hashMapConverted.containsKey("boundingbox")){
+                JSONArray boundingBoxArray = (JSONArray) hashMapConverted.get("boundingbox");
+
+                if (boundingBoxArray.size() == 4){
+                    minimumLatitude = (String) boundingBoxArray.get(0);
+                    maximumLatitude = (String) boundingBoxArray.get(1);
+                    minimumLongitude = (String) boundingBoxArray.get(2);
+                    maximumLongitude = (String) boundingBoxArray.get(3);
+                }
+            }
+
+        }
+        return new BoundingBox(minimumLatitude, maximumLatitude, minimumLongitude, maximumLongitude);
     }
+
 }
